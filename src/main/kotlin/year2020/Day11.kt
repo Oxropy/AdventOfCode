62218b23b90f6cd9969436bbfd0d6fe5bff40303
Alex <alex.pollich@t-online.de>
2020-12-14 20:08:10 +0100

    Shorten Day 11

--- src/main/kotlin/year2020/Day11.kt
+++ src/main/kotlin/year2020/Day11.kt
@@ -18,39 +18,36 @@ object Day11 : AoCApp() {
     @JvmStatic
     fun main(args: Array<String>) {
         val input = processInput(inputLines)
-        printPart(1, part1(input))
-        printPart(2, part2(input))
+        val seatWidth = input[0].count()
+        printPart(1, part1(input, seatWidth))
+        printPart(2, part2(input, seatWidth))
     }
 
-    private fun part1(seats: List<List<Char>>): String {
-        return simulate(seats, 3, ::getAdjacent).toString()
+    private fun part1(seats: List<List<Char>>, seatWidth: Int): String {
+        return getOccupiedSeatCount(simulateUntilChangeless(seats, 3, seatWidth, ::getAdjacent)).toString()
     }
 
-    private fun part2(seats: List<List<Char>>): String {
-        return simulate(seats, 4, ::getFirstSeenSeat).toString()
+    private fun part2(seats: List<List<Char>>, seatWidth: Int): String {
+        return getOccupiedSeatCount(simulateUntilChangeless(seats, 4, seatWidth, ::getFirstSeenSeat)).toString()
     }
 
     private fun processInput(lines: List<String>): List<List<Char>> {
         return lines.map { line -> line.toCharArray().toList() }
     }
 
-    private fun simulate(
+    private fun getOccupiedSeatCount(seats: List<List<Char>>): Int {
+        return seats.sumOf { line -> line.count { it == occupied } }
+    }
+
+    private fun simulateUntilChangeless(
         seats: List<List<Char>>,
         maxOccupied: Int,
+        seatWidth: Int,
         getAdjacent: (Point, List<List<Char>>, Int) -> List<Char>
-    ): Int {
-        val seatWidth = seats[0].count()
-        var oldList = listOf<List<Char>>()
-        var newList = seats
-
-        printSeats(newList)
-        while (oldList != newList) {
-            oldList = newList
-            newList = simulate(oldList, seatWidth, maxOccupied, getAdjacent)
-            printSeats(newList)
-        }
-
-        return newList.sumOf { line -> line.count { it == occupied } }
+    ): List<List<Char>> {
+        val newSeats = simulate(seats, seatWidth, maxOccupied, getAdjacent)
+        return if (newSeats == seats) newSeats
+        else simulateUntilChangeless(newSeats, maxOccupied, seatWidth, getAdjacent)
     }
 
     private fun simulate(
@@ -59,28 +56,31 @@ object Day11 : AoCApp() {
         maxOccupied: Int,
         getAdjacent: (Point, List<List<Char>>, Int) -> List<Char>
     ): List<List<Char>> {
-        val newSeats = mutableListOf<MutableList<Char>>()
-        for (y in seats.indices) {
-            val newSeatLine = mutableListOf<Char>()
-            for (x in 0 until seatWidth) {
-                val position = Point(x, y)
-                when (getPositionValue(position, seats)) {
-                    floor -> floor
-                    empty -> simulateAdjacentSeatsEmpty(position, seats, seatWidth, getAdjacent)
-                    occupied -> simulateSetEmptyWhileToManyOccupied(
-                        position,
-                        seats,
-                        seatWidth,
-                        maxOccupied,
-                        getAdjacent
-                    )
-                    else -> unreachable()
-                }.let { newSeatLine.add(it) }
+        return seats.indices.map { y ->
+            (0 until seatWidth).map { x ->
+                getSeatValueByRule(
+                    Point(x, y),
+                    seats,
+                    seatWidth,
+                    getAdjacent,
+                    maxOccupied
+                )
             }
-            newSeats.add(newSeatLine)
         }
+    }
 
-        return newSeats.toList()
+    private fun getSeatValueByRule(
+        position: Point,
+        seats: List<List<Char>>,
+        seatWidth: Int,
+        getAdjacent: (Point, List<List<Char>>, Int) -> List<Char>,
+        maxOccupied: Int
+    ) = when (getPositionValue(position, seats)) {
+        floor -> floor
+        empty -> simulateAdjacentSeatsEmpty(position, seats, seatWidth, getAdjacent)
+        occupied ->
+            simulateSetEmptyWhileToManyOccupied(position, seats, seatWidth, maxOccupied, getAdjacent)
+        else -> unreachable()
     }
 
     private fun simulateAdjacentSeatsEmpty(
@@ -89,11 +89,11 @@ object Day11 : AoCApp() {
         seatWidth: Int,
         getAdjacent: (Point, List<List<Char>>, Int) -> List<Char>
     ): Char {
-        return if (areAdjacentSeatsEmpty(position, seats, seatWidth, getAdjacent)) {
-            occupied
-        } else {
-            empty
-        }
+        return if (getAdjacent(position, seats, seatWidth)
+                .all { adjacent -> adjacent == empty || adjacent == floor || adjacent == undefined }
+        ) occupied
+        else empty
+
     }
 
     private fun simulateSetEmptyWhileToManyOccupied(
@@ -103,54 +103,45 @@ object Day11 : AoCApp() {
         maxOccupied: Int,
         getAdjacent: (Point, List<List<Char>>, Int) -> List<Char>
     ): Char {
-        return if (areToManyAdjacentSeatsOccupied(position, seats, seatWidth, maxOccupied, getAdjacent)) {
-            empty
-        } else {
-            occupied
-        }
-    }
-
-    private fun areAdjacentSeatsEmpty(
-        position: Point, seats: List<List<Char>>, seatWidth: Int,
-        getAdjacent: (Point, List<List<Char>>, Int) -> List<Char>
-    ): Boolean {
-        return getAdjacent(position, seats, seatWidth).all { adjacent -> adjacent == empty || adjacent == floor }
-    }
-
-    private fun areToManyAdjacentSeatsOccupied(
-        position: Point,
-        seats: List<List<Char>>,
-        seatWidth: Int,
-        maxOccupied: Int,
-        getAdjacent: (Point, List<List<Char>>, Int) -> List<Char>
-    ): Boolean {
-        return getAdjacent(position, seats, seatWidth).count { adjacent -> adjacent == occupied } > maxOccupied
+        return if (getAdjacent(position, seats, seatWidth)
+                .count { adjacent -> adjacent == occupied } > maxOccupied
+        ) empty
+        else occupied
     }
 
     private fun getAdjacent(position: Point, seats: List<List<Char>>, seatWidth: Int): List<Char> {
         return getAdjacentPoints.map { point -> point + position }
-            .filter { point -> point.x in 0 until seatWidth && point.y in 0 until seats.count() }
+            .filter { point -> isInRange(point, seatWidth, seats) }
             .map { index -> getPositionValue(index, seats) }
     }
 
     private fun getFirstSeenSeat(position: Point, seats: List<List<Char>>, seatWidth: Int): List<Char> {
         return getAdjacentPoints.map { direction -> getFirstSeenSeat(position, seats, seatWidth, direction) }
-            .filter { seat -> seat != undefined }
     }
 
     private fun getFirstSeenSeat(position: Point, seats: List<List<Char>>, seatWidth: Int, direction: Point): Char {
         val newPosition = position + direction
-        return if (newPosition.x in 0 until seatWidth && newPosition.y in 0 until seats.count()) {
-            val value = getPositionValue(newPosition, seats)
-            if (value == floor) {
-                getFirstSeenSeat(newPosition, seats, seatWidth, direction)
-            } else {
-                value
-            }
-        } else {
-            undefined
-        }
+        return if (isInRange(newPosition, seatWidth, seats))
+            getFirstSeenSeatValue(newPosition, seats, seatWidth, direction)
+        else undefined
     }
+
+    private fun getFirstSeenSeatValue(
+        newPosition: Point,
+        seats: List<List<Char>>,
+        seatWidth: Int,
+        direction: Point
+    ): Char {
+        val value = getPositionValue(newPosition, seats)
+        return if (value == floor) getFirstSeenSeat(newPosition, seats, seatWidth, direction)
+        else value
+    }
+
+    private fun isInRange(
+        position: Point,
+        seatWidth: Int,
+        seats: List<List<Char>>
+    ) = position.x in 0 until seatWidth && position.y in 0 until seats.count()
 
     private fun getPositionValue(position: Point, seats: List<List<Char>>): Char {
         return seats[position.y][position.x]
@@ -160,12 +151,5 @@ object Day11 : AoCApp() {
 
     operator fun Point.plus(o: Point): Point {
         return Point(x + o.x, y + o.y)
-    }
-
-    private fun printSeats(seats: List<List<Char>>) {
-        for (seatLine in seats) {
-            println(seatLine.toCharArray())
-        }
-        println()
     }
 }
