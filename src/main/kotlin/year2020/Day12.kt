b9662888e4ae869da7674b3c0ccf091edadbf8e9
Alex <alex.pollich@t-online.de>
2020-12-18 00:44:57 +0100

    Add Day 12 part 2

--- src/main/kotlin/year2020/Day12.kt
+++ src/main/kotlin/year2020/Day12.kt
@@ -6,45 +6,43 @@ import kotlin.math.abs
 object Day12 : AoCApp() {
     @JvmStatic
     fun main(args: Array<String>) {
-        val instructions = processInput(inputLines)
+        val instructions = parseInput(inputLines)
         printPart(1, part1(instructions))
-        printPart(2, part2(inputLines))
+        printPart(2, part2(instructions))
     }
 
-    private fun part1(instructions: List<Pair<String, Int>>): String {
-        return executeInstructions(instructions, 0, Position(Point(0,0), Direction.EAST)).point.let { abs(it.x) + abs(it.y) }.toString()
+    private fun part1(instructions: List<Instr>): String {
+        return executeInstructionsPart1(instructions, 0, Position(Point(0,0), Direction.EAST)).point.let { abs(it.x) + abs(it.y) }.toString()
     }
 
-    private fun part2(list: List<String>): String {
-        TODO("Not yet implemented")
+    private fun part2(instructions: List<Instr>): String {
+        return executeInstructionsPart2(instructions, 0, Position(Point(0,0), Direction.EAST), Point(10, 1)).point.let { abs(it.x) + abs(it.y) }.toString()
     }
 
-    private fun processInput(lines: List<String>): List<Pair<String, Int>> {
-        val regex = Regex("""(\w)(\d+)""")
-        return lines.map { line ->
-            regex.find(line)!!.let {
-                val (action, value) = it.destructured
-                Pair(action, value.toInt())
-            }
-        }
-    }
-
-    private fun executeInstructions(instructions: List<Pair<String, Int>>, index: Int, position: Position): Position {
-        val instruction = instructions[index]
-        val newPosition = when (instruction.first) {
-            "N" -> goDirection(position, Direction.NORTH, instruction.second)
-            "S" -> goDirection(position, Direction.SOUTH, instruction.second)
-            "E" -> goDirection(position, Direction.EAST, instruction.second)
-            "W" -> goDirection(position, Direction.WEST, instruction.second)
-            "L" -> turn(position, instruction.second, false)
-            "R" -> turn(position, instruction.second, true)
-            "F" -> goForward(position, instruction.second)
-            else -> unreachable()
+    private tailrec fun executeInstructionsPart1(instructions: List<Instr>, index: Int, position: Position): Position {
+        val newPosition = when (val instruction = instructions[index]) {
+            is Instr.Move -> goDirection(position, instruction.direction)
+            is Instr.Rotate -> turn(position, instruction.degree)
+            is Instr.MoveForward -> goForward(position, instruction.n)
         }
 
         val nextIndex = index + 1
         return if (nextIndex >= instructions.count()) newPosition
-        else executeInstructions(instructions, nextIndex, newPosition)
+        else executeInstructionsPart1(instructions, nextIndex, newPosition)
+    }
+
+    private tailrec fun executeInstructionsPart2(instructions: List<Instr>, index: Int, position: Position, waypoint: Point): Position {
+        var newPosition = position
+        var newWaypoint = waypoint
+        when (val instruction = instructions[index]) {
+            is Instr.Move -> newWaypoint = moveWaypoint(waypoint, instruction)
+            is Instr.Rotate -> newWaypoint = rotateWaypoint(waypoint, instruction.degree)
+            is Instr.MoveForward -> newPosition = goForwardToWaypoint(position, waypoint, instruction.n)
+        }
+
+        val nextIndex = index + 1
+        return if (nextIndex >= instructions.count()) newPosition
+        else executeInstructionsPart2(instructions, nextIndex, newPosition, newWaypoint)
     }
 
     enum class Direction(val dir: Int, val vector: Point) {
@@ -66,13 +64,13 @@ object Day12 : AoCApp() {
 
     data class Position(val point: Point, val direction: Direction)
 
-    private fun turn(position: Position, degree: Int, right: Boolean): Position {
-        val newDirection = getDirection(calcTurn(position.direction, degree, right))
+    private fun turn(position: Position, degree: Int): Position {
+        val newDirection = getDirection(calcTurn(position.direction, degree))
         return Position(position.point, newDirection)
     }
 
-    private fun calcTurn(direction: Direction, degree: Int, right: Boolean): Int {
-        val newDirection = direction.dir + (degree / 90) % 4 * if (right) 1 else -1
+    private fun calcTurn(direction: Direction, degree: Int): Int {
+        val newDirection = direction.dir + (degree / 90) % 4
         return when {
             newDirection > 3 -> newDirection - 4
             newDirection < 0 -> newDirection + 4
@@ -81,11 +79,32 @@ object Day12 : AoCApp() {
     }
 
     private fun goForward(position: Position, units: Int): Position {
-        return goDirection(position, position.direction, units)
+        return goDirection(position, position.direction.vector * units)
     }
 
-    private fun goDirection(position: Position, direction: Direction, units: Int): Position {
-        val newPoint = position.point + (direction.vector * units)
+    private fun goDirection(position: Position, direction: Point): Position {
+        val newPoint = position.point + direction
         return Position(newPoint, position.direction)
+    }
+
+    private fun moveWaypoint(waypoint: Point, instruction: Instr.Move) = waypoint + instruction.direction
+
+    private fun rotateWaypoint(waypoint: Point, degree: Int): Point {
+        val newDirection = ((degree / 90) + 4) % 4
+
+        var newWaypoint = waypoint
+        for (i in 1..newDirection) {
+            newWaypoint = rotate90Degree(newWaypoint)
+        }
+
+        return newWaypoint
+    }
+
+    private fun rotate90Degree(waypoint: Point): Point {
+        return Point(waypoint.y * 1, waypoint.x * -1)
+    }
+
+    private fun goForwardToWaypoint(position: Position, waypoint: Point, times: Int): Position {
+        return Position(position.point + waypoint * times, position.direction)
     }
 }
