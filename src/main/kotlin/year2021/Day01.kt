d60f8c13264c5c850fd1804720ead2379c284f94
Alex <alex.pollich@t-online.de>
2021-12-01 21:23:46 +0100

    improve Day01

--- src/main/kotlin/year2021/Day01.kt
+++ src/main/kotlin/year2021/Day01.kt
@@ -11,28 +11,17 @@ object Day01 : AoCApp() {
     }
 
     private fun part1(lines: List<Int>): String {
-
-        return addWhenCurrentValueGreaterThenOld(lines, 1, lines[0], 0).toString()
+        return addWhenCurrentValueGreaterThenOld(lines).toString()
     }
 
     private fun part2(lines: List<Int>): String {
-        val summed = lines.windowed(3, 1).map { it.sum() }
-        return addWhenCurrentValueGreaterThenOld(summed, 1, summed[0], 0).toString()
+        return addWhenCurrentValueGreaterThenOld(lines.windowed(3, 1).map { it.sum() }).toString()
     }
+
+    private fun addWhenCurrentValueGreaterThenOld(lines: List<Int>) =
+        lines.windowed(2).count { it[1] > it[0] }
 
     private fun processInput(inputLines: List<String>): List<Int> {
         return inputLines.map { l -> l.toInt() }
     }
-
-    private fun addWhenCurrentValueGreaterThenOld(values: List<Int>, index: Int, oldValue: Int, increases: Int): Int {
-        if (index >= values.size) {
-            return increases
-        }
-
-        val currentValue = values[index]
-        return addWhenCurrentValueGreaterThenOld(values, index + 1, currentValue,
-            increases + (currentValue > oldValue).toInt())
-    }
-
-    private fun Boolean.toInt() = if (this) 1 else 0
 }
