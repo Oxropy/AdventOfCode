51980f6b333adbc6572fbf7f1e25a79645a12c37
Alex <alex.pollich@t-online.de>
2021-12-03 23:48:44 +0100

    Add Day03 part 2

--- src/main/kotlin/year2021/Day03.kt
+++ src/main/kotlin/year2021/Day03.kt
@@ -6,7 +6,7 @@ object Day03 : AoCApp() {
     @JvmStatic
     fun main(args: Array<String>) {
         printPart(1, part1(inputLines))
-//        printPart(2, part2(inputLines))
+        printPart(2, part2(inputLines))
     }
 
     private fun part1(input: List<String>): String {
@@ -16,6 +16,23 @@ object Day03 : AoCApp() {
 
 
         return (gamma * epsilon).toString()
+    }
+
+    private fun part2(input: List<String>): String {
+        val oxygenGeneratorRating = findRating(input, 0, "") { countedOnes, halfLength -> (countedOnes >= halfLength).toIntChar() }
+        val co2ScrubberRating = findRating(input, 0, "") { countedOnes, halfLength -> (countedOnes < halfLength).toIntChar() }
+
+        return (oxygenGeneratorRating * co2ScrubberRating).toString()
+    }
+
+    private fun findRating(input: List<String>, column: Int, binaryFilter: String, rating: (Int, Float) -> Char): Int {
+        if (input.size == 1) {
+            return Integer.parseInt(input[0], 2)
+        }
+
+        val filter = binaryFilter + rating(input.count { value -> value[column] == '1' }, input.size / 2f)
+
+        return findRating(input.filter { it.startsWith(filter) }, column + 1, filter, rating)
     }
 
     private fun Boolean.toIntChar() = if (this) '1' else '0'
