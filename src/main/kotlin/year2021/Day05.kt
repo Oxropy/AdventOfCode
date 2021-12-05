1912f9e1ae542db97919b326c26180486306849e
Alex <alex.pollich@t-online.de>
2021-12-05 21:55:47 +0100

    Add Day05 part 2

--- src/main/kotlin/year2021/Day05.kt
+++ src/main/kotlin/year2021/Day05.kt
@@ -7,11 +7,16 @@ object Day05 : AoCApp() {
     fun main(args: Array<String>) {
         val input = processInput(inputLines)
         printPart(1, part1(input))
-//        printPart(2, part2(input))
+        printPart(2, part2(input))
     }
 
     private fun part1(input: List<Line>): String {
-        return input.flatMap { getLineFillerPointsHorizontalAndVertical(it).second }.groupBy { it }.count { it.value.size > 1 }.toString()
+        return input.flatMap { getLineFillerPoints(it).second }.groupBy { it }.count { it.value.size > 1 }.toString()
+    }
+
+    private fun part2(input: List<Line>): String {
+        return input.flatMap { getLineFillerPoints(it, false).second }.groupBy { it }.count { it.value.size > 1 }
+            .toString()
     }
 
     private fun processInput(inputLines: List<String>): List<Line> {
@@ -22,43 +27,37 @@ object Day05 : AoCApp() {
 
     private data class Line(val start: Point, val end: Point)
 
-    private fun getLineFillerPointsHorizontalAndVertical(line: Line): Pair<Line, List<Point>> {
+    private fun getLineFillerPoints(line: Line, ignoreDiagonal: Boolean = true): Pair<Line, List<Point>> {
         val isXEqual = line.start.x == line.end.x
         val isYEqual = line.start.y == line.end.y
 
         if (!isXEqual && !isYEqual) {
-            return Pair(line, listOf())
+            if (ignoreDiagonal) {
+                return Pair(line, listOf())
+            }
+
+            return getFillerPointDiagonal(line)
         }
 
         if (isXEqual) {
-            return Pair(line, if (line.start.y > line.end.y) {
-                getFillerPointsY(
-                    line.end.y,
-                    line.start.y,
-                    Point(line.end.x, line.end.y)
-                )
-            } else {
+            return Pair(
+                line,
                 getFillerPointsY(
                     line.start.y,
                     line.end.y,
-                    Point(line.start.x, line.start.y)
+                    line.start
                 )
-            })
+            )
         }
 
-        return Pair(line, if (line.start.x > line.end.x) {
-            getFillerPointsX(
-                line.end.x,
-                line.start.x,
-                Point(line.end.x, line.end.y)
-            )
-        } else {
+        return Pair(
+            line,
             getFillerPointsX(
                 line.start.x,
                 line.end.x,
-                Point(line.start.x, line.start.y)
+                line.start
             )
-        })
+        )
     }
 
     private fun getFillerPointsX(
@@ -66,7 +65,7 @@ object Day05 : AoCApp() {
         end: Int,
         startPoint: Point
     ): List<Point> {
-        return (start..end).map { startPoint.setX(it) }
+        return (start toward end).map { startPoint.setX(it) }
     }
 
     private fun getFillerPointsY(
@@ -74,6 +73,37 @@ object Day05 : AoCApp() {
         end: Int,
         startPoint: Point
     ): List<Point> {
-        return (start..end).map { startPoint.setY(it) }
+        return (start toward end).map { startPoint.setY(it) }
+    }
+
+    private fun getFillerPointDiagonal(line: Line): Pair<Line, List<Point>> {
+        val xOperand = getOperand(line.start.x, line.end.x)
+        val yOperand = getOperand(line.start.y, line.end.y)
+        val operandPoint = Point(xOperand, yOperand)
+
+        val points = mutableListOf<Point>()
+        points.add(line.start)
+        var lastPoint = line.start
+
+        while (lastPoint != line.end) {
+
+            lastPoint += operandPoint
+            points.add(lastPoint)
+        }
+
+        return Pair(line, points)
+    }
+
+    private fun getOperand(start: Int, end: Int): Int {
+        return if (start > end) {
+            -1
+        } else {
+            1
+        }
+    }
+
+    private infix fun Int.toward(to: Int): IntProgression {
+        val step = if (this > to) -1 else 1
+        return IntProgression.fromClosedRange(this, to, step)
     }
 }
