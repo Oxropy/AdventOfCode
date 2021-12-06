f9f22aad0e0d4d1f9cce662796a3fb6e0b56efc3
Alex <alex.pollich@t-online.de>
2021-12-06 18:39:58 +0100

    Update Day05

--- src/main/kotlin/year2021/Day05.kt
+++ src/main/kotlin/year2021/Day05.kt
@@ -11,11 +11,11 @@ object Day05 : AoCApp() {
     }
 
     private fun part1(input: List<Line>): String {
-        return input.flatMap { getLineFillerPoints(it).second }.groupBy { it }.count { it.value.size > 1 }.toString()
+        return input.flatMap { getLineFillerPoints(it) }.groupBy { it }.count { it.value.size > 1 }.toString()
     }
 
     private fun part2(input: List<Line>): String {
-        return input.flatMap { getLineFillerPoints(it, false).second }.groupBy { it }.count { it.value.size > 1 }
+        return input.flatMap { getLineFillerPoints(it, false) }.groupBy { it }.count { it.value.size > 1 }
             .toString()
     }
 
@@ -27,56 +27,40 @@ object Day05 : AoCApp() {
 
     private data class Line(val start: Point, val end: Point)
 
-    private fun getLineFillerPoints(line: Line, ignoreDiagonal: Boolean = true): Pair<Line, List<Point>> {
+    private fun getLineFillerPoints(line: Line, ignoreDiagonal: Boolean = true): List<Point> {
         val isXEqual = line.start.x == line.end.x
         val isYEqual = line.start.y == line.end.y
 
         if (!isXEqual && !isYEqual) {
             if (ignoreDiagonal) {
-                return Pair(line, listOf())
+                return listOf()
             }
 
             return getFillerPointDiagonal(line)
         }
 
         if (isXEqual) {
-            return Pair(
-                line,
-                getFillerPointsY(
-                    line.start.y,
-                    line.end.y,
-                    line.start
-                )
-            )
+            return getFillerPoints(
+                line.start.y,
+                line.end.y
+            ) { newValue -> line.start.setY(newValue) }
         }
 
-        return Pair(
-            line,
-            getFillerPointsX(
-                line.start.x,
-                line.end.x,
-                line.start
-            )
-        )
+        return getFillerPoints(
+            line.start.x,
+            line.end.x
+        ) { newValue -> line.start.setX(newValue) }
     }
 
-    private fun getFillerPointsX(
+    private fun getFillerPoints(
         start: Int,
         end: Int,
-        startPoint: Point
+        setValue: (Int) -> Point
     ): List<Point> {
-        return (start toward end).map { startPoint.setX(it) }
+        return (start toward end).map { setValue(it) }
     }
 
-    private fun getFillerPointsY(
-        start: Int,
-        end: Int,
-        startPoint: Point
-    ): List<Point> {
-        return (start toward end).map { startPoint.setY(it) }
-    }
-
-    private fun getFillerPointDiagonal(line: Line): Pair<Line, List<Point>> {
+    private fun getFillerPointDiagonal(line: Line): List<Point> {
         val xOperand = getOperand(line.start.x, line.end.x)
         val yOperand = getOperand(line.start.y, line.end.y)
         val operandPoint = Point(xOperand, yOperand)
@@ -91,7 +75,7 @@ object Day05 : AoCApp() {
             points.add(lastPoint)
         }
 
-        return Pair(line, points)
+        return points
     }
 
     private fun getOperand(start: Int, end: Int): Int {
