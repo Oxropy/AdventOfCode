7ca01061aadfcb67ba181122049092e8d0cdfb06
Alex <alex.pollich@t-online.de>
2021-12-07 22:23:39 +0100

    Add Day07 part 2

--- src/main/kotlin/year2021/Day07.kt
+++ src/main/kotlin/year2021/Day07.kt
@@ -14,18 +14,21 @@ object Day07: AoCApp() {
         val minValue = input.minOf { it }
         val average = input.sum() / input.size
 
-        return (minValue .. average).minOf { calculateMovementCost(input, it) }.toString()
+        return (minValue .. average).minOf { calculateMovementCost(input, it){ v -> v } }.toString()
     }
 
     private fun part2(input: List<Int>): String {
-        TODO("Not yet implemented")
+        val minValue = input.minOf { it }
+        val average = input.sum() / input.size
+
+        return (minValue .. average).minOf { calculateMovementCost(input, it){ v -> (v * (v + 1)) / 2 } }.toString()
     }
 
     private fun processInput(lines: String): List<Int> {
         return lines.split(',').map { it.toInt() }
     }
 
-    private fun calculateMovementCost(values: List<Int>, to: Int): Int {
-        return values.sumOf { maxOf(it, to) - minOf(it, to) }
+    private fun calculateMovementCost(values: List<Int>, to: Int, addToMovement: (Int) -> Int): Int {
+        return values.sumOf { addToMovement(maxOf(it, to) - minOf(it, to)) }
     }
 }
