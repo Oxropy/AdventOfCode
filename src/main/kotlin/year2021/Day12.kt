b6920d99ca75a79327618682bb1dfe125e4d2ae2
Alex <alex.pollich@t-online.de>
2021-12-13 20:45:17 +0100

    Add Day12 part 2

--- src/main/kotlin/year2021/Day12.kt
+++ src/main/kotlin/year2021/Day12.kt
@@ -2,7 +2,7 @@ package year2021
 
 import AoCApp
 
-object Day12: AoCApp() {
+object Day12 : AoCApp() {
     @JvmStatic
     fun main(args: Array<String>) {
         val input = processInput(inputLines)
@@ -10,41 +10,60 @@ object Day12: AoCApp() {
         printPart(2, part2(input))
     }
 
-    private fun part1(caveMap:Map<Cave, List<Cave>>): String {
+    private fun part1(caveMap: Map<Cave, List<Cave>>): String {
         val start = caveMap.keys.first { it.name == "start" }
         return findPaths(caveMap, listOf(), listOf(start), caveMap[start]!!).size.toString()
     }
 
     private fun part2(caveMap: Map<Cave, List<Cave>>): String {
-        TODO("Not yet implemented")
+        val start = caveMap.keys.first { it.name == "start" }
+        return findPaths(caveMap, listOf(), listOf(start), caveMap[start]!!, true).size.toString()
     }
 
     private fun processInput(input: List<String>): Map<Cave, List<Cave>> {
-        val pairs = input.map { it.split('-').let { (from, to) -> Pair(createCave(from), createCave(to)) } }
+        val pairs = input.map { it.split('-').let { (from, to) -> Pair(Cave(from), Cave(to)) } }
         val reversePair = pairs.map { Pair(it.second, it.first) }
-        return (pairs + reversePair).groupBy( { it.first }, {it.second})
+        return (pairs + reversePair).groupBy({ it.first }, { it.second })
     }
 
-    private fun findPaths(caveMap: Map<Cave, List<Cave>>, paths: List<List<Cave>>, path: List<Cave>, caves: List<Cave>): List<List<Cave>> {
-        return caves.flatMap { findPaths(caveMap, paths, path, it) }
+    private fun findPaths(
+        caveMap: Map<Cave, List<Cave>>,
+        paths: List<List<Cave>>,
+        path: List<Cave>,
+        caves: List<Cave>,
+        allowSmallTwice: Boolean = false
+    ): List<List<Cave>> {
+        return caves.flatMap { findPaths(caveMap, paths, path, it, allowSmallTwice) }
     }
 
-    private fun findPaths(caveMap: Map<Cave, List<Cave>>, paths: List<List<Cave>>, path: List<Cave>, cave: Cave): List<List<Cave>> {
+    private fun findPaths(
+        caveMap: Map<Cave, List<Cave>>,
+        paths: List<List<Cave>>,
+        path: List<Cave>,
+        cave: Cave,
+        allowSmallTwice: Boolean
+    ): List<List<Cave>> {
         if (cave.name == "end") {
             val newPath = path + cave
             return paths + listOf(newPath)
         }
 
-        if (!cave.isBig && path.contains(cave)) {
-            return emptyList()
+        if (!cave.isBig) {
+            if (allowSmallTwice && cave.name != "start") {
+                if (path.filter { !it.isBig }.groupBy { it }.any { it.value.size == 2 } && path.contains(cave)) {
+                    return emptyList()
+                }
+            } else {
+                if (path.contains(cave)) {
+                    return emptyList()
+                }
+            }
         }
 
-        return caveMap[cave]!!.flatMap { findPaths(caveMap, paths, path + cave, it) }
+        return caveMap[cave]!!.flatMap { findPaths(caveMap, paths, path + cave, it, allowSmallTwice) }
     }
 
-    private fun createCave(name: String): Cave {
-        return Cave(name, name[0].isUpperCase())
+    private data class Cave(val name: String, val isBig: Boolean) {
+        constructor(name: String) : this(name, name[0].isUpperCase())
     }
-
-    private data class Cave(val name: String, val isBig: Boolean)
 }
