e2fe20bbfaf5ab89eee05fae2ca58d732d491284
Alex <alex.pollich@t-online.de>
2021-12-18 21:49:43 +0100

    Add Day14 part 2 does not terminate

--- src/main/kotlin/year2021/Day14.kt
+++ src/main/kotlin/year2021/Day14.kt
@@ -11,24 +11,33 @@ object Day14: AoCApp() {
     }
 
     private fun part1(input: Pair<String, Map<String, String>>): String {
-        val insertions = input.second
-        val polymer = (1..10).fold(input.first) { acc, _ ->
-            acc.windowed(2).joinToString("") { it[0] + insertions[it]!! } + acc.last()
-        }
-
-        val occurrence = polymer.groupBy { it }.map { Pair(it.key, it.value.size) }
-        val mostCommon = occurrence.maxOf { it.second }
-        val leastCommon = occurrence.minOf { it.second }
-        return (mostCommon - leastCommon).toString()
+        val polymer = runInsertions(input, 10)
+        return calculateResult(polymer)
     }
 
     private fun part2(input: Pair<String, Map<String, String>>): String {
-        TODO("Not yet implemented")
+        val polymer = runInsertions(input, 40)
+        return calculateResult(polymer)
     }
 
     private fun processInput(inputLines: List<List<String>>): Pair<String, Map<String, String>> {
         val template = inputLines[0][0]
         val insertions = inputLines[1].associate { line -> line.split(" -> ").let { (pair, insertion) -> Pair(pair, insertion) } }
         return Pair(template, insertions)
+    }
+
+    private fun runInsertions(input: Pair<String, Map<String, String>>, steps: Int): String {
+        val insertions = input.second
+        val polymer = (1..steps).fold(input.first) { acc, _ ->
+            acc.windowed(2).joinToString("") { it[0] + insertions[it]!! } + acc.last()
+        }
+        return polymer
+    }
+
+    private fun calculateResult(polymer: String): String {
+        val occurrence = polymer.groupBy { it }.map { Pair(it.key, it.value.size) }
+        val mostCommon = occurrence.maxOf { it.second }
+        val leastCommon = occurrence.minOf { it.second }
+        return (mostCommon - leastCommon).toString()
     }
 }
