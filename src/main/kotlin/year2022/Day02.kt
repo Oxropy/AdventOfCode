abd37a89834ce24542d27391075d7a0b8ef31627
Alex <alex.pollich@t-online.de>
2022-12-02 16:31:33 +0100

    Add 2022 Day02 part 2

--- src/main/kotlin/year2022/Day02.kt
+++ src/main/kotlin/year2022/Day02.kt
@@ -10,22 +10,33 @@ object Day02 : AoCApp() {
         printPart(2, part2(input))
     }
 
-    private fun part1(rounds: List<Pair<Shape, Shape>>): String {
-       return rounds.sumOf { round -> play(round.first, round.second) + round.second.value }.toString()
+    private fun part1(rounds: List<Pair<String, String>>): String {
+        return rounds.map { value -> Pair(getShape(value.first), getShape(value.second)) }
+            .sumOf { round -> play(round.first, round.second).value + round.second.value }.toString()
     }
 
-    private fun part2(rounds: List<Pair<Shape, Shape>>): String {
-        TODO("Not yet implemented")
+    private fun part2(rounds: List<Pair<String, String>>): String {
+        return rounds.map { value ->
+            val enemyShape = getShape(value.first)
+            Pair(enemyShape, getMatchingShape(enemyShape, getHowToEnd(value.second)))
+        }
+            .sumOf { round -> play(round.first, round.second).value + round.second.value }.toString()
     }
 
-    private fun processInput(inputLines: List<String>): List<Pair<Shape, Shape>> {
-        return inputLines.map { line -> line.split(" ").map { value -> getShape(value) }.let { Pair(it[0], it[1]) } }
+    private fun processInput(inputLines: List<String>): List<Pair<String, String>> {
+        return inputLines.map { line -> line.split(" ").let { Pair(it[0], it[1]) } }
     }
 
     enum class Shape(val value: Int) {
         Rock(1),
         Paper(2),
         Scissor(3)
+    }
+
+    enum class HowToEnd(val value: Int) {
+        Win(6),
+        Draw(3),
+        Lose(0)
     }
 
     private fun getShape(value: String): Shape {
@@ -40,22 +51,55 @@ object Day02 : AoCApp() {
         }
     }
 
-    private fun play(enemy: Shape, player: Shape): Int {
-        return when (enemy){
-            Shape.Rock -> when (player){
-                Shape.Rock -> 3
-                Shape.Paper -> 6
-                Shape.Scissor -> 0
+    private fun getHowToEnd(value: String): HowToEnd {
+        return when (value) {
+            "X" -> HowToEnd.Lose
+            "Y" -> HowToEnd.Draw
+            "Z" -> HowToEnd.Win
+            else -> unreachable()
+        }
+    }
+
+    private fun getMatchingShape(enemy: Shape, howToEnd: HowToEnd): Shape {
+        return when (enemy) {
+            Shape.Rock -> when (howToEnd) {
+                HowToEnd.Lose -> Shape.Scissor
+                HowToEnd.Draw -> Shape.Rock
+                HowToEnd.Win -> Shape.Paper
             }
-            Shape.Paper -> when (player){
-                Shape.Rock -> 0
-                Shape.Paper -> 3
-                Shape.Scissor -> 6
+
+            Shape.Paper -> when (howToEnd) {
+                HowToEnd.Lose -> Shape.Rock
+                HowToEnd.Draw -> Shape.Paper
+                HowToEnd.Win -> Shape.Scissor
             }
-            Shape.Scissor -> when (player){
-                Shape.Rock -> 6
-                Shape.Paper -> 0
-                Shape.Scissor -> 3
+
+            Shape.Scissor -> when (howToEnd) {
+                HowToEnd.Lose -> Shape.Paper
+                HowToEnd.Draw -> Shape.Scissor
+                HowToEnd.Win -> Shape.Rock
+            }
+        }
+    }
+
+    private fun play(enemy: Shape, player: Shape): HowToEnd {
+        return when (enemy) {
+            Shape.Rock -> when (player) {
+                Shape.Rock -> HowToEnd.Draw
+                Shape.Paper -> HowToEnd.Win
+                Shape.Scissor -> HowToEnd.Lose
+            }
+
+            Shape.Paper -> when (player) {
+                Shape.Rock -> HowToEnd.Lose
+                Shape.Paper -> HowToEnd.Draw
+                Shape.Scissor -> HowToEnd.Win
+            }
+
+            Shape.Scissor -> when (player) {
+                Shape.Rock -> HowToEnd.Win
+                Shape.Paper -> HowToEnd.Lose
+                Shape.Scissor -> HowToEnd.Draw
             }
         }
     }
