5c58ac0c258c9dfc2d847bf3db46752a25dfb9b2
Alex <alex.pollich@t-online.de>
2022-12-03 18:19:24 +0100

    Add 2022 Day03 part 1

---
+++ src/main/kotlin/year2022/Day03.kt
@@ -0,0 +1,36 @@
+package year2022
+
+import AoCApp
+
+object Day03 : AoCApp() {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = processInput(inputLines)
+        printPart(1, part1(input))
+        printPart(2, part2(input))
+    }
+
+    private fun part1(input: List<String>): String {
+        return input.sumOf { line ->
+            val firstCompartment = line.substring(0, line.length / 2).toCharArray()
+            val secondCompartment = line.substring(line.length / 2).toCharArray()
+            val inBoth = firstCompartment.intersect(secondCompartment.asIterable().toSet()).first()
+            getPriority(inBoth)
+        }.toString()
+    }
+
+    private fun part2(input: List<String>): String {
+        TODO("Not yet implemented")
+    }
+
+    private fun processInput(inputLines: List<String>): List<String> {
+        return inputLines
+    }
+
+    private fun getPriority(value: Char): Int {
+        return if (value.isUpperCase())
+            (value.code - 'A'.code) + 27
+        else
+            (value.code - 'a'.code) + 1
+    }
+}
