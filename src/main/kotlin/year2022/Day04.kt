ce52597389cb117e55d9cbb492a9fe6ea0dd4a4b
Alex <alex.pollich@t-online.de>
2022-12-04 17:25:28 +0100

    Add 2022 Day04 part 2

--- src/main/kotlin/year2022/Day04.kt
+++ src/main/kotlin/year2022/Day04.kt
@@ -10,20 +10,27 @@ object Day04 : AoCApp() {
         printPart(2, part2(input))
     }
 
-    private fun part1(input: List<Pair<List<Int>,List<Int>>>): String {
+    private fun part1(input: List<Pair<List<Int>, List<Int>>>): String {
         return input.count { isListFullyIntersect(it.first, it.second) }.toString()
     }
 
-    private fun part2(input: List<Pair<List<Int>,List<Int>>>): String {
-        TODO("Not yet implemented")
+    private fun part2(input: List<Pair<List<Int>, List<Int>>>): String {
+        return input.count { isListIntersect(it.first, it.second) }.toString()
     }
 
-    private fun processInput(inputLines: List<String>): List<Pair<List<Int>,List<Int>>> {
-        return inputLines.map { line -> "(\\d+)-(\\d+),(\\d+)-(\\d+)".toRegex().matchEntire(line)!!.groupValues.drop(1).map { it.toInt() }.let { Pair((it[0]..it[1]).toList(), (it[2]..it[3]).toList()) } }
+    private fun processInput(inputLines: List<String>): List<Pair<List<Int>, List<Int>>> {
+        return inputLines.map { line ->
+            "(\\d+)-(\\d+),(\\d+)-(\\d+)".toRegex().matchEntire(line)!!.groupValues.drop(1).map { it.toInt() }
+                .let { Pair((it[0]..it[1]).toList(), (it[2]..it[3]).toList()) }
+        }
     }
 
-    private fun isListFullyIntersect(first: List<Int>, second: List<Int>) : Boolean{
+    private fun isListFullyIntersect(first: List<Int>, second: List<Int>): Boolean {
         val intersect = first.intersect(second.toSet())
         return intersect.size == first.size || intersect.size == second.size
+    }
+
+    private fun isListIntersect(first: List<Int>, second: List<Int>): Boolean {
+        return first.intersect(second.toSet()).isNotEmpty()
     }
 }
