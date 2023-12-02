4dc58841b66ee71dc33623a46d0579f9c29fe35c
Alex <alex.pollich@t-online.de>
2023-12-02 16:17:53 +0100

    Add 2023 Day02 part 1

---
+++ src/main/kotlin/year2023/Day02.kt
@@ -0,0 +1,56 @@
+package year2023
+
+import AoCApp
+
+object Day02: AoCApp() {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = parseGames(inputLines)
+        printPart(1, part1(input))
+//        printPart(2, part2(input))
+    }
+
+    private fun part1(games: List<Game>): String {
+        return games.filter { isPossible(it, 12, 13, 14) }.sumOf { it.id }.toString()
+    }
+
+    private fun part2(games: List<Game>): String {
+        TODO("Not yet implemented")
+    }
+
+    private fun isPossible(game: Game, maxRed: Int, maxGreen: Int, maxBlue: Int): Boolean {
+        return !game.subsets.any { it.red > maxRed || it.green > maxGreen || it.blue > maxBlue }
+    }
+
+    private fun parseGames(input: List<String>): List<Game> {
+        return input.map { parseGame(it) }
+    }
+
+    private fun parseGame(line: String): Game {
+        val subsetStart = line.indexOf(':')
+        val id = line.substring(4, subsetStart).trim().toInt()
+        val subsets = mutableListOf<Subset>()
+        for (subset in line.substring(subsetStart + 1).split(";")) {
+            val cubes = subset.split(",")
+            var red = 0
+            var green = 0
+            var blue = 0
+            for (cube in cubes) {
+                val colors = cube.split(" ")
+                val count = colors[1].trim().toInt()
+                when (colors[2]) {
+                    "red" -> red = count
+                    "green" -> green = count
+                    "blue" -> blue = count
+                }
+            }
+
+            subsets.add(Subset(red, green, blue))
+        }
+
+        return Game(id, subsets)
+    }
+
+    data class Game(val id: Int, val subsets: List<Subset>)
+    data class Subset(val red: Int, val green: Int, val blue: Int)
+}
