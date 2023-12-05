2b725ec0540616b76678ab18cac253037dd0e0db
Alex <alex.pollich@t-online.de>
2023-12-05 23:18:12 +0100

    Add 2023 Day05 part 2 (WIP)

--- src/main/kotlin/year2023/Day05.kt
+++ src/main/kotlin/year2023/Day05.kt
@@ -7,11 +7,39 @@ object Day05 : AoCApp() {
     fun main(args: Array<String>) {
         val input = inputGroupedLines
         printPart(1, part1(input))
-//        printPart(2, part2(input))
+        printPart(2, part2(input))
     }
 
     private fun part1(input: List<List<String>>): String {
         val seeds = input[0][0].split(" ").drop(1).map { it.toLong() }.toSet()
+        val almanac = getAlmanac(input)
+
+        return seeds.minOf { seed -> seedToLocation(seed, almanac) }.toString()
+    }
+
+    private fun part2(input: List<List<String>>): String {
+        val seedRanges = input[0][0].split(" ").drop(1).map { it.toLong() }.zipWithNext().filterIndexed { index, _ -> index % 2 == 0 }.toSet()
+        val almanac = getAlmanac(input)
+
+        return seedRanges.minOf { seedRange ->
+            (seedRange.first until seedRange.first + seedRange.second).minOf { seed -> seedToLocation(seed, almanac) }
+        }.toString()
+    }
+
+    private fun seedToLocation(
+        seed: Long,
+        almanac: List<List<Entry>>
+    ): Long {
+        val soil = getDestinationOrDefault(seed, almanac[0])
+        val fertilizer = getDestinationOrDefault(soil, almanac[1])
+        val water = getDestinationOrDefault(fertilizer, almanac[2])
+        val light = getDestinationOrDefault(water, almanac[3])
+        val temperature = getDestinationOrDefault(light, almanac[4])
+        val humidity = getDestinationOrDefault(temperature, almanac[5])
+        return getDestinationOrDefault(humidity, almanac[6])
+    }
+
+    private fun getAlmanac(input: List<List<String>>): List<List<Entry>> {
         val seedToSoil = sourceToDestinationMap(input[1])
         val soilToFertilizer = sourceToDestinationMap(input[2])
         val fertilizerToWater = sourceToDestinationMap(input[3])
@@ -19,20 +47,15 @@ object Day05 : AoCApp() {
         val lightToTemperature = sourceToDestinationMap(input[5])
         val temperatureToHumidity = sourceToDestinationMap(input[6])
         val humidityToLocation = sourceToDestinationMap(input[7])
-
-        return seeds.minOf { seed ->
-            val soil = getDestinationOrDefault(seed, seedToSoil)
-            val fertilizer = getDestinationOrDefault(soil, soilToFertilizer)
-            val water = getDestinationOrDefault(fertilizer, fertilizerToWater)
-            val light = getDestinationOrDefault(water, waterToLight)
-            val temperature = getDestinationOrDefault(light, lightToTemperature)
-            val humidity = getDestinationOrDefault(temperature, temperatureToHumidity)
-            getDestinationOrDefault(humidity, humidityToLocation)
-        }.toString()
-    }
-
-    private fun part2(input: List<List<String>>): String {
-        TODO("Not yet implemented")
+        return listOf(
+            seedToSoil,
+            soilToFertilizer,
+            fertilizerToWater,
+            waterToLight,
+            lightToTemperature,
+            temperatureToHumidity,
+            humidityToLocation
+        )
     }
 
     private fun sourceToDestinationMap(mapping: List<String>): List<Entry> {
