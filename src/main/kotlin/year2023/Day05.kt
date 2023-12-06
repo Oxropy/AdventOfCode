95ebe0decf37197babca8ab2a130f25429a47258
Alex <alex.pollich@t-online.de>
2023-12-06 21:28:54 +0100

    Add 2023 Day05 part 2 (WIP)

--- src/main/kotlin/year2023/Day05.kt
+++ src/main/kotlin/year2023/Day05.kt
@@ -6,37 +6,37 @@ object Day05 : AoCApp() {
     @JvmStatic
     fun main(args: Array<String>) {
         val input = inputGroupedLines
-        printPart(1, part1(input))
+//        printPart(1, part1(input))
         printPart(2, part2(input))
     }
 
     private fun part1(input: List<List<String>>): String {
-        val seeds = input[0][0].split(" ").drop(1).map { it.toLong() }.toSet()
+        val seeds = input[0][0].split(" ").drop(1).map { IdRange(it.toLong(), 1) }.toSet()
         val almanac = getAlmanac(input)
 
-        return seeds.minOf { seed -> seedToLocation(seed, almanac) }.toString()
+        return seeds.minOf { seed -> seedRangeToLocation(seed, almanac).minOf { it.source } }.toString()
     }
 
     private fun part2(input: List<List<String>>): String {
-        val seedRanges = input[0][0].split(" ").drop(1).map { it.toLong() }.zipWithNext().filterIndexed { index, _ -> index % 2 == 0 }.toSet()
+        val seedRanges = input[0][0].split(" ").asSequence().drop(1)
+            .map { it.toLong() }.zipWithNext().filterIndexed { index, _ -> index % 2 == 0 }.map { IdRange(it.first, it.second) }.toSet()
         val almanac = getAlmanac(input)
 
-        return seedRanges.minOf { seedRange ->
-            (seedRange.first until seedRange.first + seedRange.second).minOf { seed -> seedToLocation(seed, almanac) }
-        }.toString()
+        return seedRanges.minOf { seed -> seedRangeToLocation(seed, almanac).minOf { it.source } }.toString()
     }
 
-    private fun seedToLocation(
-        seed: Long,
+    private fun seedRangeToLocation(
+        seedRange: IdRange,
         almanac: List<List<Entry>>
-    ): Long {
-        val soil = getDestinationOrDefault(seed, almanac[0])
+    ): List<IdRange> {
+        val soil = getDestinationOrDefault(seedRange, almanac[0])
         val fertilizer = getDestinationOrDefault(soil, almanac[1])
         val water = getDestinationOrDefault(fertilizer, almanac[2])
         val light = getDestinationOrDefault(water, almanac[3])
         val temperature = getDestinationOrDefault(light, almanac[4])
         val humidity = getDestinationOrDefault(temperature, almanac[5])
-        return getDestinationOrDefault(humidity, almanac[6])
+        val location = getDestinationOrDefault(humidity, almanac[6])
+        return location
     }
 
     private fun getAlmanac(input: List<List<String>>): List<List<Entry>> {
@@ -70,16 +70,37 @@ object Day05 : AoCApp() {
         }
     }
 
-    private fun getDestinationOrDefault(source: Long, mappings: List<Entry>): Long {
-        mappings.forEach {
-            val index = source - it.source
-            if (index >= 0 && index <= it.length) {
-                return it.destination + index
-            }
-        }
-
-        return source
+    private fun getDestinationOrDefault(ranges: List<IdRange>, mappings: List<Entry>): List<IdRange> {
+        return ranges.flatMap { getDestinationOrDefault(it, mappings) }
     }
 
+    private fun getDestinationOrDefault(range: IdRange, mappings: List<Entry>): List<IdRange> {
+        val result = mutableListOf<IdRange>()
+        var lastRange = range
+        var mappingIndex = 0
+        while (mappingIndex < mappings.size) {
+            val mapping = mappings[mappingIndex]
+            val index = lastRange.source - mapping.source
+            if (index >= 0 && index <= mapping.length) {
+                if (mapping.length - index >= lastRange.length) {
+                    result.add(IdRange(mapping.destination + index, lastRange.length))
+                    return result
+                } else {
+                    val splitRange = IdRange(mapping.destination + index, lastRange.length - (mapping.length - index))
+                    result.add(splitRange)
+                    lastRange = IdRange(lastRange.source + splitRange.length, lastRange.length - splitRange.length)
+                    mappingIndex = 0
+                    continue
+                }
+            }
+
+            mappingIndex++
+        }
+
+        result.add(lastRange)
+        return result
+    }
+
+    data class IdRange(val source: Long, val length: Long)
     data class Entry(val source: Long, val destination: Long, val length: Long)
 }
