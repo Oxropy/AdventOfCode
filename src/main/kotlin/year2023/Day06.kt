d90d304ec0440cf72b7262d7da1765ed0d0e32ec
Alex <alex.pollich@t-online.de>
2023-12-06 22:35:21 +0100

    Add 2023 Day06 part 2

--- src/main/kotlin/year2023/Day06.kt
+++ src/main/kotlin/year2023/Day06.kt
@@ -7,12 +7,12 @@ object Day06: AoCApp() {
     fun main(args: Array<String>) {
         val input = inputLines
         printPart(1, part1(input))
-//        printPart(2, part2(input))
+        printPart(2, part2(input))
     }
 
     private fun part1(input: List<String>): String {
-        val time = input[0].drop(11).split(' ').filter { it.isNotEmpty() }.map { it.trim().toInt() }
-        val distance = input[1].drop(11).split(' ').filter { it.isNotEmpty() }.map { it.trim().toInt() }
+        val time = input[0].drop(11).split(' ').filter { it.isNotEmpty() }.map { it.trim().toLong() }
+        val distance = input[1].drop(11).split(' ').filter { it.isNotEmpty() }.map { it.trim().toLong() }
         val records = time.indices.map { Pair(time[it], distance[it]) }
 
         return records.map { record ->
@@ -26,10 +26,18 @@ object Day06: AoCApp() {
     }
 
     private fun part2(input: List<String>): String {
-        TODO("Not yet implemented")
+        val time = input[0].drop(11).filter { it.isDigit() }.toLong()
+        val distance = input[1].drop(11).filter { it.isDigit() }.toLong()
+
+        return (1 until time).count { holdTime ->
+                calculateDistance(
+                    time,
+                    holdTime
+                ) > distance
+            }.toString()
     }
 
-    private fun calculateDistance(time: Int, holdTime: Int): Int {
+    private fun calculateDistance(time: Long, holdTime: Long): Long {
         return (time - holdTime) *  holdTime
     }
 }
