063ff96c099a70e329e576c65cd1114538b8c710
Alex <alex.pollich@t-online.de>
2023-12-19 23:13:36 +0100

    Add 2023 Day10 part 2

--- src/main/kotlin/year2023/Day10.kt
+++ src/main/kotlin/year2023/Day10.kt
@@ -7,23 +7,55 @@ object Day10 : AoCApp() {
     fun main(args: Array<String>) {
         val input = inputLines
         printPart(1, part1(input))
-//        printPart(2, part2(input))
+        printPart(2, part2(input))
     }
 
     private fun part1(input: List<String>): String {
-        val pipes = parseInput(input)
-        val startPoint = getStartPoint(pipes)
-        return (getPipeLoop(pipes, startPoint).size / 2).toString()
+        val tiles = parseInput(input)
+        return (getPipeLoop(tiles).size / 2).toString()
     }
 
     private fun part2(input: List<String>): String {
-        TODO("Not yet implemented")
+        val tiles = parseInput(input)
+        val route = getPipeLoop(tiles)
+        val startPipeStyle = getStartPipeStyle(route)
+
+        val top = route.minOf { it.y }
+        val left = route.minOf { it.x }
+
+        val bottom = route.maxOf { it.y }
+        val right = route.maxOf { it.x }
+
+        val correctedTiles = tiles.map { rows -> rows.map { if (it is Tile.Start) Tile.Pipe(startPipeStyle) else it } }
+
+        var enclosedCount = 0
+        for (y in top..bottom) {
+            for (x in left..right) {
+                if (Point(x, y) in route) {
+                    continue
+                }
+                fun countIntersections(xCoords: Iterable<Int>): Int {
+                    return xCoords
+                        .map { Point(it, y) }
+                        .filter { it in route }
+                        .map { correctedTiles[it.y][it.x] }
+                        .count { it is Tile.Pipe && it.style.isVertical() && it.style !in setOf(PipeStyle.SouthAndEast, PipeStyle.SouthAndWest) }
+                }
+                val toLeft = countIntersections(left..<x)
+
+                if (toLeft % 2 == 1) {
+                    enclosedCount++
+                }
+            }
+        }
+
+        return enclosedCount.toString()
     }
 
-    private fun getPipeLoop(pipes: List<List<Tile>>, startPoint: Point): List<Tile> {
-        val routeStart = listOf(pipes[startPoint.y][startPoint.x])
+    private fun getPipeLoop(tiles: List<List<Tile>>): List<Point> {
+        val startPoint = getStartPoint(tiles)
         for (direction in listOf(Direction.DOWN, Direction.RIGHT, Direction.UP, Direction.LEFT)) {
-            val loop = findLoop(pipes, startPoint + direction.vector, direction, routeStart)
+            val loop = findLoop(tiles, startPoint + direction.vector, direction, emptyList())
             if (loop.isNotEmpty()) {
                 return loop
             }
@@ -36,25 +68,31 @@ object Day10 : AoCApp() {
         pipes: List<List<Tile>>,
         point: Point,
         direction: Direction,
-        route: List<Tile>
-    ): List<Tile> = when (val currentTile = pipes[point.y][point.x]) {
-        is Tile.Start ->
-            route
+        route: List<Point>
+    ): List<Point> {
+        if (point.x < 0 || point.y < 0 || point.x >= pipes[0].size || point.y >= pipes.size) {
+            return emptyList()
+        }
+        return when (val currentTile = pipes[point.y][point.x]) {
+            is Tile.Start ->
+                route + point
 
-        is Tile.Ground ->
-            emptyList()
-
-        is Tile.Pipe -> {
-            val nextDirection = currentTile.style.outputFor(direction.opposite())
-            if (nextDirection != null) {
-                findLoop(
-                    pipes,
-                    point + nextDirection,
-                    nextDirection,
-                    route + currentTile
-                )
-            } else {
+            is Tile.Ground ->
                 emptyList()
+
+            is Tile.Pipe -> {
+                val nextDirection = currentTile.style.outputFor(direction.opposite())
+                if (nextDirection != null) {
+                    val nextPoint = point + nextDirection
+                    findLoop(
+                        pipes,
+                        nextPoint,
+                        nextDirection,
+                        route + point,
+                    )
+                } else {
+                    emptyList()
+                }
             }
         }
     }
@@ -75,7 +113,6 @@ object Day10 : AoCApp() {
         }
     }
 
-
     private fun getStartPoint(pipes: List<List<Tile>>): Point {
         for (y in pipes.indices) {
             for (x in pipes[y].indices) {
@@ -88,9 +125,35 @@ object Day10 : AoCApp() {
         unreachable()
     }
 
+    private fun getStartPipeStyle(route: List<Point>): PipeStyle {
+        val start = route[route.size - 1]
+        val before = route[route.size - 2]
+        val after = route[0]
+
+        fun f(point1: Point, point2: Point): PipeStyle? {
+
+            val firstDirection = point1 - start
+            val lastDirection = point2 - start
+
+            return when {
+                firstDirection.x == 0 && firstDirection.y == 1 && lastDirection.x == 0 && lastDirection.y == -1 -> PipeStyle.Vertical
+                firstDirection.x == 1 && firstDirection.y == 0 && lastDirection.x == -1 && lastDirection.y == 0 -> PipeStyle.Horizontal
+                firstDirection.x == 0 && firstDirection.y == -1 && lastDirection.x == -1 && lastDirection.y == 0 -> PipeStyle.NorthAndWest
+                firstDirection.x == 0 && firstDirection.y == -1 && lastDirection.x == 1 && lastDirection.y == 0 -> PipeStyle.NorthAndEast
+                firstDirection.x == 0 && firstDirection.y == 1 && lastDirection.x == 1 && lastDirection.y == 0 -> PipeStyle.SouthAndEast
+                firstDirection.x == 0 && firstDirection.y == 1 && lastDirection.x == -1 && lastDirection.y == 0 -> PipeStyle.SouthAndWest
+                else -> null
+            }
+        }
+
+        return f(before, after)
+            ?: f(after, before)
+            ?: unreachable()
+    }
+
     enum class PipeStyle(
-        val input1: Direction,
-        val input2: Direction,
+        private val input1: Direction,
+        private val input2: Direction,
     ) {
         Vertical(Direction.DOWN, Direction.UP),
         Horizontal(Direction.LEFT, Direction.RIGHT),
@@ -103,6 +166,15 @@ object Day10 : AoCApp() {
             input1 == direction -> input2
             input2 == direction -> input1
             else -> null
+        }
+
+        fun isVertical() = when (this) {
+            Vertical -> true
+            Horizontal -> false
+            NorthAndEast -> true
+            NorthAndWest -> true
+            SouthAndEast -> true
+            SouthAndWest -> true
         }
     }
 
