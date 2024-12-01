bae93452b5d71b20b40eeba7f10a20780897aaa4
Alex <alex.pollich@t-online.de>
2024-12-01 21:41:21 +0100

    Add 2024 Day01 part 1

---
+++ src/main/kotlin/year2024/Day01.kt
@@ -0,0 +1,30 @@
+package year2024
+
+import AoCApp
+import kotlin.math.abs
+
+object Day01: AoCApp() {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = parseGames(inputLines)
+        printPart(1, part1(input.first, input.second))
+//        printPart(2, part2(input.first, input.second))
+    }
+
+    private fun part1(list1: List<Int>, list2: List<Int>): String {
+        val sorted1 = list1.sorted()
+        val sorted2 = list2.sorted()
+        return sorted1.zip(sorted2).sumOf { (a, b) -> abs(a - b) }.toString()
+    }
+
+    private fun part2(list1: List<Int>, list2: List<Int>): String {
+        TODO("Not yet implemented")
+    }
+
+    private fun parseGames(input: List<String>): Pair<List<Int>, List<Int>> {
+        val pairs = input.map { row -> row.split("   ").let { Pair(it[0].trim().toInt(), it[1].trim().toInt()) } }
+        val first = pairs.map { it.first }
+        val second = pairs.map { it.second }
+        return Pair(first, second)
+    }
+}
