ccfac119bb66b9a2a049019e5a786b39c67837c9
Alex <alex.pollich@t-online.de>
2024-12-05 23:35:39 +0100

    Add 2024 Day05 part 2

--- src/main/kotlin/year2024/Day05.kt
+++ src/main/kotlin/year2024/Day05.kt
@@ -5,12 +5,62 @@ import AoCApp
 object Day05 : AoCApp() {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = inputGroupedLines
+        val input = parseInput(inputGroupedLines)
         printPart(1, part1(input))
         printPart(2, part2(input))
     }
 
-    private fun part1(input: List<List<String>>): String {
+    private fun part1(input: Pair<Set<Pair<Int, Int>>, List<List<Int>>>): String {
+        return input.second.sumOf { line ->
+            line.windowed(2).all { (a, b) -> input.first.contains(Pair(a, b)) }.let {
+                if (it) {
+                    val middle = line.size / 2
+                    line[middle]
+                } else {
+                    0
+                }
+            }
+        }.toString()
+    }
+
+    private fun part2(input: Pair<Set<Pair<Int, Int>>, List<List<Int>>>): String {
+        return input.second.sumOf { line ->
+            line.windowed(2).any { (a, b) -> input.first.contains(Pair(b, a)) }.let {
+                if (it) {
+                    val middle = line.size / 2
+                    val reorderedPages = reorderPage(input.first, line)
+                    reorderedPages[middle]
+                } else {
+                    0
+                }
+            }
+        }.toString()
+    }
+
+    private fun reorderPage(order: Set<Pair<Int, Int>>, pages: List<Int>): List<Int> {
+        val mutableMapOf = mutableMapOf<Int, Int>()
+        for (i in pages.indices) {
+            val a = pages[i]
+            mutableMapOf[a] = 1
+
+            for (j in pages.indices) {
+                if (i == j) {
+                    continue
+                }
+
+                val b = pages[j]
+                val abPair = Pair(a, b)
+
+                if (order.contains(abPair) && mutableMapOf.containsKey(a)) {
+                    mutableMapOf[a] = mutableMapOf[a]!!.plus(1)
+                }
+            }
+        }
+
+        return mutableMapOf.toList().sortedBy { (_, v) -> v }.toMap().keys.toList()
+    }
+
+    private fun parseInput(input: List<List<String>>): Pair<Set<Pair<Int, Int>>, List<List<Int>>> {
         val pageOrder = input.first().map { line ->
             line.split('|').let {
                 val first = it[0].toInt()
@@ -19,20 +69,7 @@ object Day05 : AoCApp() {
             }
         }.toHashSet()
 
-        return input[1].sumOf { line ->
-            val pages = line.split(',').map { it.toInt() }
-            pages.windowed(2).all { (a, b) -> pageOrder.contains(Pair(a, b)) }.let {
-                if (it) {
-                    val middle = pages.size / 2
-                    pages[middle]
-                } else {
-                    0
-                }
-            }
-        }.toString()
-    }
-
-    private fun part2(input: List<List<String>>): String {
-        TODO("Not yet implemented")
+        val pages = input[1].map { line -> line.split(',').map { it.toInt() } }
+        return Pair(pageOrder, pages)
     }
 }
